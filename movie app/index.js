52bd32ab1730110fbad43fa75758e7e76856390f
Pavel <you@example.com>
2022-02-12 02:00:46 +0300

    feat: add videos info pages

--- movie app/index.js
+++ movie app/index.js
@@ -3,6 +3,7 @@ const gallery = document.querySelector('.gallery')
 const home = document.querySelector('.home')
 const body = document.querySelector('body')
 const inp = document.querySelector('input')
+let isMovie = false
 let link = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top'
 
 inp.value = ''
@@ -16,7 +17,7 @@ inp.value = ''
 // }
 // window.addEventListener('beforeunload', setLocalStorage)
 
-async function getData(lin) {
+async function getData(lin, isMovie=false) {
 
     //search
     //`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${keyword}`
@@ -29,21 +30,43 @@ async function getData(lin) {
         },
     });
     const data = await res.json();
-    showData(data);
+    showData(data, isMovie);
 }
 getData(link);
 
-function showData(data) {
-    data.films.forEach((el, ind) => {
+function showData(data, isMovie=false) {
+    if (isMovie === true) {
+        let bg = `<div class="rev-bg items">`
+        let div1 = `<div class="rev-img" style='background-image: url("${data.posterUrl}")'>`
+        let div2 = `<div class="rev-info">`
+        let h2 = `<h2>${data.nameRu} (${data.year})</h2>`
+        let p1
+        if (data.shortDescription === null) {
+            p1 = `<p>Описание отсутствует</p>`
+        } else {
+            p1 = `<p>Описание: ${data.shortDescription}</p>`
+        }
+        let temp = ''
+        for (let keys in data.genres) {
+            temp = `${temp} ${data.genres[keys].genre},`
+        }
+        let p2 = `<p>Жанры:${temp}</p>`
+        gallery.insertAdjacentHTML('beforeend', bg);
+        document.querySelector('.rev-bg').insertAdjacentHTML('beforeend', div1)
+        document.querySelector('.rev-bg').insertAdjacentHTML('beforeend', div2)
+        document.querySelector('.rev-info').insertAdjacentHTML('beforeend', h2)
+        document.querySelector('.rev-info').insertAdjacentHTML('beforeend', p1)
+        document.querySelector('.rev-info').insertAdjacentHTML('beforeend', p2)
+    } else {
+        data.films.forEach((el, ind) => {
         for (let keys in el) {
-            // let img = `<img class="gallery-img${ind} items" src="${el.posterUrlPreview}" alt="image${ind}">`;
             let color
             if (el.rating >= 8) {
                 color = 'lightgreen'
             } else if (el.rating >= 6) {
                 color = 'orange'
             } else color = 'red'
-            let div1 = `<div class="gallery-img${ind} items" style='background-image: url("${el.posterUrlPreview}")'>`;
+            let div1 = `<div class="gallery-img${ind} items" id='${el.filmId}' style='background-image: url("${el.posterUrlPreview}")'>`;
             let div2 = `<div class="movie-info${ind} movie-info">`
             let p1 = `<p class="name">${el.nameRu}</p>`
             let p2
@@ -64,10 +87,12 @@ function showData(data) {
         let p = `<p class="error">К сожалению, поиск не дал результатов</p>`
         gallery.insertAdjacentHTML('beforeend', p);
     }
+    }
     // console.log(data)
 }
 
 home.addEventListener('click', function () {
+    isMovie = false
     let items = document.querySelectorAll('.items')
     items.forEach(e => {
         e.remove()
@@ -83,6 +108,7 @@ home.addEventListener('click', function () {
 
 inp.addEventListener('keydown', function (event) {
     if (event.code === 'Enter') {
+        isMovie = false
         let items = document.querySelectorAll('.items')
         items.forEach(e => {
             e.remove()
@@ -93,6 +119,25 @@ inp.addEventListener('keydown', function (event) {
         })
         link = `https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${inp.value}`
         getData(link);
-        inp.value = ''
     }
 })
+
+gallery.onclick = function openMovie(event) {
+    if (event.target.classList.contains('items')) {
+        isMovie = true
+        let items = document.querySelectorAll('.items')
+        items.forEach(e => {
+            e.remove()
+        })
+        items = document.querySelectorAll('.error')
+        items.forEach(e => {
+            e.remove()
+        })
+        link = `https://kinopoiskapiunofficial.tech/api/v2.2/films/${event.path[0].id}`
+        getData(link, isMovie);
+        // console.log(isMovie)
+        // console.log(event.path[0].id)
+    }
+}
+
+//genres
